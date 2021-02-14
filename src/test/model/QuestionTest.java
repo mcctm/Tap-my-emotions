a31e4b33f0e07d2f5ae4cb917e6316c5db273dba
mirandachan <mc835@student.ubc.ca>
2021-02-13 21:07:26 -0800

    added testing for getQuestionPrompt and getQuestionAnswer

--- src/test/model/QuestionTest.java
+++ src/test/model/QuestionTest.java
@@ -54,19 +54,21 @@ class QuestionTest {
 
         QuestionBank questionBank = new QuestionBank();
 
-        String playerQuestion = "Susan worked for the whole day. How would she feel?";
-        String playerAnswer = "Tired";
-        questionBank.addQuestion(playerQuestion, playerAnswer);
+        String playerQuestion0 = "Susan worked for the whole day. How would she feel?";
+        String playerAnswer0 = "Tired";
+        questionBank.addQuestion(playerQuestion0, playerAnswer0);
 
-        String playerQuestion2 = "Someone broke your favourite cup. How would you feel?";
-        String playerAnswer2 = "Angry";
-        questionBank.addQuestion(playerQuestion2, playerAnswer2);
+        String playerQuestion1 = "Someone broke your favourite cup. How would you feel?";
+        String playerAnswer1 = "Angry";
+        questionBank.addQuestion(playerQuestion1, playerAnswer1);
 
-        assertTrue(questionBank.checkAnswer(playerAnswer, 0));
-        assertFalse(questionBank.checkAnswer(playerAnswer2, 0));
+        assertTrue(questionBank.getQuestionPrompt(0).equals(playerQuestion0));
+        assertTrue(questionBank.checkAnswer(questionBank.getQuestionAnswer(0), 0));
+        assertFalse(questionBank.checkAnswer(questionBank.getQuestionAnswer(1), 0));
 
-        assertTrue(questionBank.checkAnswer(playerAnswer2, 1));
-        assertFalse(questionBank.checkAnswer(playerAnswer, 1));
+        assertTrue(questionBank.getQuestionPrompt(1).equals(playerQuestion1));
+        assertTrue(questionBank.checkAnswer(questionBank.getQuestionAnswer(1), 1));
+        assertFalse(questionBank.checkAnswer(questionBank.getQuestionAnswer(0), 1));
 
     }
 
